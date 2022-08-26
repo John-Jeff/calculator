0a734adeba76f4f33bb28d7c284438426653e6d9
jeff <jeffreyjohn102@gmail.com>
2022-08-26 16:02:25 -0400

    Modify collectInput function and create operate function

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let num2 = [];
 let operation;
 let result;
 
-buttonBox.addEventListener('click', function(e) {
+buttonBox.addEventListener('click', function (e) {
     if (e.target !== this) {
         collectInput(e.target);
         inputText.textContent += e.target.value;
@@ -19,30 +19,25 @@ buttonBox.addEventListener('click', function(e) {
 
 function collectInput(input) {
 
-    if (input.value === '/') {
-       computation(input);
-    }
-    else if (input.value === '*') {
-        computation(input);
-    }
-    else if (input.value === '-') {
-        computation(input);
-    }
-    else if (input.value === '+') {
-        computation(input);
-    }
+    if (input.classList.contains('op')) computation(input);
+    // else if (input.value === '*') computation(input);
+    // else if (input.value === '-') computation(input);
+    // else if (input.value === '+') computation(input);
     else if (input.value === '=') {
         if (numbers.length > 0) {
             num2 = parseInt(numbers.join(''));
-            if (operation === '/') result = num1/num2;
-            if (operation === '*') result = num1*num2;
-            if (operation === '-') result = num1-num2;
-            if (operation === '+') result = num1+num2;
+            if (operation === '/') {
+                // if (num1 < num2) result = parseFloat((num1/num2).toFixed(15));
+                result = num1 / num2;
+            }
+            else if (operation === '*') result = num1 * num2;
+            else if (operation === '-') result = num1 - num2;
+            else if (operation === '+') result = num1 + num2;
         }
     }
 
     else {
-        if(parseInt(input.value) === NaN) {}
+        if (parseInt(input.value) === NaN) { }
         else {
             numbers.push(parseInt(input.value));
             console.log(numbers);
@@ -55,11 +50,24 @@ function computation(input) {
     if (numbers.length > 0) {
 
         num1 = parseInt(numbers.join(''));
-        
+
         for (let i = 0; i <= numbers.length; i++) {
             numbers.shift();
         };
 
         operation = input.value;
     }
+}
+
+function operate(num1, num2, op) {
+    let firstNum = num1;
+    let secNum = num2;
+    let result;
+
+    if (op === '/') result = firstNum / secNum;
+    else if (op === '*') result = firstNum * secNum;
+    else if (op === '-') result = firstNum - secNum;
+    else if (op === '+') result = firstNum + secNum;
+
+    return result;
 }
