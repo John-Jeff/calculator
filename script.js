1ea95ad0fb4d94c940b44459a89d73d5da8659d7
jeff <jeffreyjohn102@gmail.com>
2022-08-26 13:21:15 -0400

    Initialize division functionality

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
 let buttonBox = document.querySelector('#button-box');
+let inputText = document.querySelector('#input');
+let resultText = document.querySelector('#result');
 
 let numbers = [];
+let num1 = [];
+let num2 = [];
+let result = 0;
 
 buttonBox.addEventListener('click', function(e) {
-    collectInput(e.target)
+    if (e.target !== this) {
+        collectInput(e.target);
+        inputText.textContent += e.target.value;
+        resultText.textContent = result;
+    }
+
 })
 
 function collectInput(input) {
 
-    if (input.value === '/') console.log('/');
-    else if (input.value === '*') console.log('*');
+    if (input.value === '/') {
+        if (numbers.length > 0) {
+
+            num1 = numbers.join('');
+            
+            for (let i = 0; i <= numbers.length; i++) {
+                numbers.shift();
+            };
+        }
+    }
+    else if (input.value === '*') {
+
+    }
     else if (input.value === '-') console.log('-');
     else if (input.value === '+') console.log('+');
-    else if (input.value === '=') console.log('=');
+    else if (input.value === '=') {
+        if (numbers.length > 0) {
+            num2 = numbers.join('');
+            result = num1/num2;
+        }
+    }
 
     else {
         if(parseInt(input.value) === NaN) {}
