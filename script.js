f3ff716eec1514297a7135e7b009ea8b2b8b48ca
jeff <jeffreyjohn102@gmail.com>
2022-08-28 13:11:50 -0400

    Remove unused code

--- script.js
+++ script.js
@@ -2,90 +2,31 @@ const buttonBox = document.querySelector('#button-box');
 const inputText = document.querySelector('#input');
 const resultText = document.querySelector('#result');
 
-const calcSys = ['+', '-', '÷', '✕', '=', 'on', 'ac', 'spec'];
+const calcSys = ['on', 'ac', 'spec'];
 const calcFunc = ['+', '-', '÷', '✕', '='];
 const calcArray = [];
 let numbers = [];
-let num1 = [];
-let num2 = [];
-let operation;
 let result;
 
 buttonBox.addEventListener('click', function (e) {
     if (e.target !== this) {
         if (e.target.value === 'ac') {
+            calcArray.splice(0, calcArray.length);
             inputText.textContent = '';
             result = '';
         }
 
-        // collectInput(e.target);
-        test(e.target);
-        inputText.textContent += e.target.value;
+        operate(e.target);
+        if (e.target.value !== '=' && !calcSys.includes(e.target.value)) inputText.textContent += e.target.value;
         resultText.textContent = result;
     }
 })
 
-function collectInput(input) {
-
-    if (calcSys.includes(input.value)) {
-        if (numbers.length > 0) {
-
-            num1 = parseInt(numbers.join(''));
-
-            for (let i = 0; i <= numbers.length; i++) numbers.shift();
-
-            operation = input.value;
-        }
-    }
-
-    else if (input.value === '=') {
-        if (numbers.length > 0) {
-            num2 = parseInt(numbers.join(''));
-
-            result = operate(operation, num1, num2);
-        }
-    }
-
-    else if (!calcSys.includes(input.value)) {
-        numbers.push(parseInt(input.value));
-        console.log(numbers);
-    }
-}
-
-function operate(op, ...num) {
-    let firstNum = num[0];
-    let secNum = num[1];
-    let result;
-
-    if (typeof result === 'number') firstNum = result;
-
-    switch (op) {
-        case '÷':
-            result = firstNum / secNum;
-            break;
-        case '✕':
-            result = firstNum * secNum;
-            break;
-        case '-':
-            result = firstNum - secNum;
-            break;
-        case '+':
-            result = firstNum + secNum;
-            break;
-    }
-    // if (op === '÷') result = firstNum / secNum;
-    // else if (op === '✕') result = firstNum * secNum;
-    // else if (op === '-') result = firstNum - secNum;
-    // else if (op === '+') result = firstNum + secNum;
-
-    return result;
-}
-
 // if (num1 < num2) result = parseFloat((num1/num2).toFixed(15));
 
-function test(input) {
+function operate(input) {
 
-    if (!calcSys.includes(input.value)) {
+    if (!calcFunc.includes(input.value) && !calcSys.includes(input.value)) {
         numbers.push(parseInt(input.value));
         console.log(numbers);
     }
