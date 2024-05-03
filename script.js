040232ee3e9b51d3211abc87610cddd9a89d5f67
Jeff <jeffreyjohn102@gmail.com>
2024-05-03 17:12:04 -0400

    Remove old functions and variables

--- script.js
+++ script.js
@@ -5,12 +5,6 @@ const clearBtn = document.querySelector('#all-clear');
 const inputText = document.querySelector('#input');
 const resultText = document.querySelector('#result');
 
-const calcSys = ['on', 'ac', 'spec'];
-const calcFunc = ['+', '-', '÷', '✕', '='];
-const calcArray = [];
-const numbers = [];
-let result;
-
 let value1 = '';
 let value2 = '';
 let operator = '';
@@ -80,55 +74,3 @@ function clear() {
     operator = '';
     updateDisplay();
 }
-
-// buttonBox.addEventListener('click', function (e) {
-//     if (e.target !== this) {
-//         if (e.target.value === 'ac') {
-//             calcArray.splice(0, calcArray.length);
-//             inputText.textContent = '';
-//             result = '';
-//         }
-
-//         operate(e.target);
-//         if (e.target.value !== '=' && !calcSys.includes(e.target.value)) inputText.textContent += e.target.value;
-//         resultText.textContent = result;
-//     }
-// })
-
-// function operate(input) {
-
-//     if (!calcFunc.includes(input.value) && !calcSys.includes(input.value)) {
-//         numbers.push(parseInt(input.value));
-//         // console.log(numbers);
-//     }
-
-//     if (calcFunc.includes(input.value)) {
-
-//         calcArray.push(parseInt(numbers.join('')));
-//         for (let i = 0; i <= numbers.length; i++) numbers.shift();
-//         calcArray.push(input.value);
-
-//         if (calcArray.length >= 3) {
-//             switch (calcArray[1]) {
-//                 case '÷':
-//                     calcArray[0] = calcArray[0] / calcArray[2];
-//                     break;
-//                 case '✕':
-//                     calcArray[0] = calcArray[0] * calcArray[2];
-//                     break;
-//                 case '-':
-//                     calcArray[0] = calcArray[0] - calcArray[2];
-//                     break;
-//                 case '+':
-//                     calcArray[0] = calcArray[0] + calcArray[2];
-//                     break;
-//             }
-
-//             (calcArray[3] === '=') ? calcArray.splice(1, calcArray.length - 1) : calcArray.splice(1, calcArray.length - 2);
-            
-//             // calcArray[0] = parseFloat(calcArray[0]).toFixed(10);
-
-//             result = calcArray[0];
-//         }
-//     }
-// }
